7c303e9a4362670206a1f1b0326f57635524b6d8
Casey James <casey@khaosophy.com>
2022-08-08 00:12:03 -0400

    astro init and markup structure

---
+++ src/content/donate.js
@@ -0,0 +1,20 @@
+const donate = [
+  {
+    title: 'Climate Change Issue',
+    description: 'We are fighting for a better world for all children',
+    link: {
+      to: '#',
+      label: 'Donate Now',
+    },
+  },
+  {
+    title: 'Feeding NJ',
+    description: 'Help feed people in our own backyards',
+    link: {
+      to: '#',
+      label: 'Donate Now',
+    },
+  },
+];
+
+export default donate;
