db1f5c163f719a9c6ffddd44e63f066a925e0444
Casey James Perno <casey@khaosophy.com>
2022-08-09 16:41:54 -0400

    re-ordered charity list

--- src/content/donate.js
+++ src/content/donate.js
@@ -1,21 +1,5 @@
 const donate = [
   {
-    title: 'Ocean Conservancy',
-    description: 'Our ocean unifies our world like nothing else. And saving it from ongoing threats is a cause that affects us all.',
-    link: {
-      to: 'https://donate.oceanconservancy.org/page/92465/donate/1',
-      label: 'Donate Now',
-    },
-  },
-  {
-    title: 'Sierra Club Foundation',
-    description: 'The Sierra Club Foundation promotes climate solutions, conservation, and grassroots advocacy.',
-    link: {
-      to: 'https://act.sierraclub.org/donate/rc_connect__campaign_designform?id=70131000001hPsPAAU&formcampaignid=701310000008koOAAQ',
-      label: 'Donate Now',
-    },
-  },
-  {
     title: 'NJ Community Food Bank',
     description: 'Connecting people with food and ending hunger in our own backyard.',
     link: {
@@ -24,10 +8,26 @@ const donate = [
     },
   },
   {
+    title: 'Ocean Conservancy',
+    description: 'Our ocean unifies our world like nothing else. And saving it from ongoing threats is a cause that affects us all.',
+    link: {
+      to: 'https://donate.oceanconservancy.org/page/92465/donate/1',
+      label: 'Donate Now',
+    },
+  },
+  {
     title: 'Ferst Readers',
     description: 'A child\'s path to a brighter future is paved with books.',
     link: {
       to: 'https://ferst-readers-inc.networkforgood.com/projects/149522-donate-now',
+      label: 'Donate Now',
+    },
+  },
+  {
+    title: 'Sierra Club Foundation',
+    description: 'The Sierra Club Foundation promotes climate solutions, conservation, and grassroots advocacy.',
+    link: {
+      to: 'https://act.sierraclub.org/donate/rc_connect__campaign_designform?id=70131000001hPsPAAU&formcampaignid=701310000008koOAAQ',
       label: 'Donate Now',
     },
   },
