e9ade9236c668f42465a6e7a4f671bc4404e9045
Casey Perno <caseyp@partyrentalltd.com>
2023-08-21 11:11:12 -0400

    create rules page

---
+++ src/content/rules.js
@@ -0,0 +1,11 @@
+const rules = [
+  {
+    title: 'While Driving, One Must Always Be in a Properly Secured Car Seat',
+  },
+  {
+    title: 'No Rolling on the Changing Table',
+    // description: 'Connecting people with food and ending hunger in our own backyard.',
+  },
+];
+
+export default rules;
