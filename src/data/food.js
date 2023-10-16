43a7d5df665cc8414e4020a765d73a09d0b9ca66
Casey Perno <casey@khaosophy.com>
2023-10-16 14:57:58 -0400

    food data

---
+++ src/data/food.js
@@ -0,0 +1,20 @@
+/**
+ * TODO: 
+ * * Add more items
+ * * Add "rating" property, or something like that, to determine how much she likes it
+ */
+
+const food = [
+  {
+    name: 'Blueberries',
+    type: 'Fruit',
+    hasTried: true,
+  },
+  {
+    name: 'Strawberries',
+    type: 'Fruit',
+    hasTried: true,
+  },
+];
+
+export default food;
